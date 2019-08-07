042d019cf04da1f77503a2fc1059333317a96bd3
Kevin-269581661 <269581661@qq.com>
2019-08-07 11:18:06 +0800

    搭建脚手架配置

---
+++ vue-puzzle-verification/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/demo'
+    },
+    {
+      path: '/demo',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+  ]
+})
